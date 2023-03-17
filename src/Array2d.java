be93bab56fc88f74deed1c8aad478d65ab6559da
AHDesigns <anthony.hancock85@gmail.com>
2023-03-17 22:55:39 +0000

    ArraySW5

--- src/Array2d.java
+++ src/Array2d.java
@@ -2,16 +2,16 @@ public class Array2d {
     //2 Dimension Array = array with multiple columns and rows
     public static void main(String[] args) {
         String [][] students = {
-                {"Harry", "Potter"},
-                {"Ron", "Weasley"},
-                {"Hermoine", "Grainger"}
+                {"Harry", "Potter", "M"},
+                {"Ron", "Weasley", "M"},
+                {"Hermoine", "Grainger", "F"}
         };
 
         for (int i=0; i < students.length; i++) {
             for (int j=0; j < students[i].length; j++) {
                 System.out.println(students[i][j] + " ");
             }
-            System.out.print(" ");
+            System.out.print("");
         }
     }
 }
