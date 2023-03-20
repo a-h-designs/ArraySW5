941da0533a079085fabb55f5964163106e828b26
AHDesigns <anthony.hancock85@gmail.com>
2023-03-20 23:51:35 +0000

    ArraySW5

--- src/Array2d.java
+++ src/Array2d.java
@@ -1,17 +1,48 @@
+import java.util.Scanner;
+
 public class Array2d {
     //2 Dimension Array = array with multiple columns and rows
     public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
         String [][] students = {
-                {"Harry", "Potter", "M"},
-                {"Ron", "Weasley", "M"},
-                {"Hermoine", "Grainger", "F"}
+                {"Harry", "Potter", "Gryffindor"},
+                {"Draco", "Malfoy", "Slytherin"},
+                {"Newton", "Scamander", "Hufflepuff"},
+                {"Luna", "Lovegood", "Ravenclaw"}
         };
 
-        for (int i=0; i < students.length; i++) {
-            for (int j=0; j < students[i].length; j++) {
-                System.out.println(students[i][j] + " ");
+        int option = 0;
+        System.out.println("Enter a number and we will see if it's in our database.");
+        String guess = reader.next();
+
+        for (int i = 0; i < students.length; i++) {
+            if (guess.equalsIgnoreCase("Harry")) {
+                option = 1;
+                break;
+            } else if (guess.equalsIgnoreCase("Draco")) {
+                option = 2;
+                break;
+            } else if (guess.equalsIgnoreCase("Newton")) {
+                option = 3;
+                break;
+            } else if (guess.equalsIgnoreCase("Luna")) {
+                option = 4;
+                break;
             }
-            System.out.print("");
+        }
+
+        if (option == 1) {
+            System.out.print(students[0][0] + " " + students[0][1] + " - " + students[0][2]);
+        } else if (option == 2) {
+            System.out.print(students[1][0] + " " + students[1][1] + " - " + students[1][2]);
+        } else if (option == 3) {
+            System.out.print(students[2][0] + " " + students[2][1] + " - " + students[2][2]);
+        } else if (option == 4) {
+            System.out.print(students[3][0] + " " + students[3][1] + " - " + students[3][2]);
+        } else {
+            System.out.print("Not found.");
         }
     }
 }
--- src/ExistinArray.java
+++ src/ExistinArray.java
@@ -6,7 +6,7 @@ public class ExistinArray {
         Scanner reader = new Scanner(System.in);
         //create int array with 10 numbers inside
         int[] numbers = {2, 3, 5, 11, 16, 19, 20, 22, 36, 49};
-        boolean userGuess = false;
+        boolean valueFind = false;
         //ask user to guess the number
         System.out.println("Enter a number and we will see if it's in our database.");
         //save user input, int guess = reader.nextInt()
@@ -14,12 +14,12 @@ public class ExistinArray {
         //use for loop to access your array, if number[1] == guess, you found the number
         for (int number : numbers) {
             if (guess == number) {
-                userGuess = true;
+                valueFind = true;
                 break;
             }
         }
         //print if guess not available, say your guess is incorrect, otherwise say you guessed the numbers.
-        if (userGuess) {
+        if (valueFind) {
             System.out.println(guess + " has been found in our database");
         } else {
             System.out.println(guess + " has not been found in our database");
